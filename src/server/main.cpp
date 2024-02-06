9bc5770ce8e0c861c8ffe090013da6310293105a
Theodoros Nikolaou <nikolaoutheod@gmail.com>
2024-02-06 06:39:35 +0100

    Remove magic numbers

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -14,16 +14,18 @@ int main()
 
 
     PositionService positionService(zmqPublisher);
-    for (size_t sensor_id = 0; sensor_id < 1; sensor_id++) 
+    constexpr auto NUMBER_OF_SENSORS = 10;
+    for (size_t sensor_id = 0; sensor_id < NUMBER_OF_SENSORS; sensor_id++)
     {
         PositionSensor positionSensor(sensor_id, data3dGenerator);
         positionService.registerSensor(positionSensor);
     }
 
+    constexpr auto SENSOR_SAMPLING_IN_MS = 1000;
     while (true)
     {
         positionService.update();
         positionService.publish();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(SENSOR_SAMPLING_IN_MS));
     }
 }
